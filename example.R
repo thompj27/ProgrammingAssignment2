bbd3c145552c68b37fc370d0fbe69cdcf432694d
John Thompson <john.thompson@bms.com>
2015-10-25 19:44:33 -0400

    JRT ProgrammingAssignment 2  Solution : 25Oct2015

---
+++ example.R
@@ -0,0 +1,37 @@
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+
+m <- diag(nrow = 5, ncol = 5)
+m[1,2]=1
+m[1,3]=5
+m[1,1]=10
+n = solve(m)
+m
+n
+
+m
+
